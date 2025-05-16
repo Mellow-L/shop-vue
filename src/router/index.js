509386bbcd3214e3e03222082009e02a47f21e79
linyiran <1245991813@qq.com>
2025-05-16 22:00:29 +0800

    feat：实现路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -3,45 +3,26 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 // 导入页面组件
 import Layout from '@/components/Layout.vue'; // 导入统一布局
-import Login from '@/views/common/Login.vue'
-import Register from '@/views/user/Register.vue'
-import Shop from '@/views/user/Shop.vue'
-import MyCart from '@/views/user/MyCart.vue'
-import MyOrders from '@/views/user/MyOrders.vue'
-import MyProfile from '@/views/user/MyProfile.vue'
-import ProductDetail from '@/views/user/ProductDetail.vue'
-import OrderDetail from '@/views/user/OrderDetail.vue'
-import ChangePassword from '@/views/user/ChangePassword.vue'
-import OrderConfirmation from '@/views/user/OrderConfirmation.vue'
-
-// admin
-import ManageOrders from '@/views/admin/ManageOrders.vue'
-import ManageProducts from '@/views/admin/ManageProducts.vue'
-import ManageUsers from '@/views/admin/ManageUsers.vue'
-import ProductAdd from '@/views/admin/ProductAdd.vue'
-import ProductModify from '@/views/admin/ProductModify.vue'
-import OrderDetailManage from '@/views/admin/OrderDetailManage.vue'
-import UserDetailManage from '@/views/admin/UserDetailManage.vue'
 
 // 路由配置
 const routes = [
   // 公共页面
-  { path: '/login', name: 'Login', component: Login },
-  { path: '/register', name: 'Register', component: Register },
+  { path: '/login', name: 'Login', component: () => import('@/views/common/Login.vue') },
+  { path: '/register', name: 'Register', component: () => import('@/views/user/Register.vue') },
 
   // 带 Layout 的用户页面
   {
     path: '/',
     component: Layout,
     children: [
-      { path: '', name: 'Shop', component: Shop },
-      { path: '/cart', name: 'MyCart', component: MyCart },
-      { path: '/orders', name: 'MyOrders', component: MyOrders },
-      { path: '/profile', name: 'MyProfile', component: MyProfile },
-      { path: '/product/:id', name: 'ProductDetail', component: ProductDetail },
-      { path: '/order/:id', name: 'OrderDetail', component: OrderDetail },
-      { path: '/change-password', name: 'ChangePassword', component: ChangePassword },
-      { path: '/confirm-order', name: 'OrderConfirmation', component: OrderConfirmation },
+      { path: '', name: 'Shop', component: () => import('@/views/user/Shop.vue') },
+      { path: '/cart', name: 'MyCart', component: () => import('@/views/user/MyCart.vue') },
+      { path: '/orders', name: 'MyOrders', component: () => import('@/views/user/MyOrders.vue') },
+      { path: '/profile', name: 'MyProfile', component: () => import('@/views/user/MyProfile.vue') },
+      { path: '/product/:id', name: 'ProductDetail', component: () => import('@/views/user/ProductDetail.vue') },
+      { path: '/order/:id', name: 'OrderDetail', component: () => import('@/views/user/OrderDetail.vue') },
+      { path: '/change-password', name: 'ChangePassword', component: () => import('@/views/user/ChangePassword.vue') },
+      { path: '/confirm-order', name: 'OrderConfirmation', component: () => import('@/views/user/OrderConfirmation.vue') },
     ],
   },
 
@@ -50,13 +31,13 @@ const routes = [
     path: '/admin',
     component: Layout,
     children: [
-      { path: '/admin/orders', name: 'ManageOrders', component: ManageOrders },
-      { path: '/admin/orders/:id', name: 'AdminOrderDetail', component: OrderDetailManage },
-      { path: '/admin/products', name: 'ManageProducts', component: ManageProducts },
-      { path: '/admin/users', name: 'ManageUsers', component: ManageUsers },
-      { path: '/admin/user/:id', name: 'AdminUserDetail', component: UserDetailManage, props: true },
-      { path: '/admin/product/add', name: 'ProductAdd', component: ProductAdd },
-      { path: '/admin/product/modify/:id', name: 'ProductModify', component: ProductModify },
+      { path: '/admin/orders', name: 'ManageOrders', component: () => import('@/views/admin/ManageOrders.vue') },
+      { path: '/admin/orders/:id', name: 'AdminOrderDetail', component: () => import('@/views/admin/OrderDetailManage.vue') },
+      { path: '/admin/products', name: 'ManageProducts', component: () => import('@/views/admin/ManageProducts.vue') },
+      { path: '/admin/users', name: 'ManageUsers', component: () => import('@/views/admin/ManageUsers.vue') },
+      { path: '/admin/user/:id', name: 'AdminUserDetail', component: () => import('@/views/admin/UserDetailManage.vue'), props: true },
+      { path: '/admin/product/add', name: 'ProductAdd', component: () => import('@/views/admin/ProductAdd.vue') },
+      { path: '/admin/product/modify/:id', name: 'ProductModify', component: () => import('@/views/admin/ProductModify.vue') },
     ],
   },
 ];
